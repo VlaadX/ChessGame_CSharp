1c85c34d26a7db15d7c7e8b485517b9a2dd57b6c
Wladimir Gadelha <WladimirgadelhaJOB@gmail.com>
2024-06-11 16:34:32 -0300

    Commit da versao inicial do Tabuleiro e algumas logicas

---
+++ ChessLogic/Player.cs
@@ -0,0 +1,23 @@
+﻿namespace ChessLogic
+{
+    public enum Player
+    {
+        None, White, Black
+    }
+
+    public static class PlayerExtensions
+    {
+        public static Player Oponnent(this Player player)
+        {
+            switch (player)
+            {
+                case Player.White:
+                    return Player.Black;
+                case Player.Black:
+                    return Player.White;
+                default: return Player.None;
+
+            }
+        }
+    }
+}
